5405798a5fe6bbf2dd8154edd7d34cf8ebd66be4
Student Space <niloufarrahimizade@yahoo.com>
2021-06-10 17:36:15 +0430

    last commit

---
+++ review2.py
@@ -0,0 +1,8 @@
+inp1 = int(input("Please enter a number: "))
+
+if inp1 > 10:
+    print("inp is bigger")
+elif inp1 < 10:
+    print("inp is smaller")
+else:
+    print("equal")
---
+++ review3.py
@@ -0,0 +1,12 @@
+name = "alibaba"
+list_name = list()
+while True:
+    inp1 = input("Please enter a name: ")
+    list_name.append(inp1)
+    print(list_name)
+    if inp1 == "finish":
+        break
+
+
+
+
